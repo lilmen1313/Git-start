ca5f1feb957393e6cd1d8137642f4c2678ea53c0
lilmen <lilmen1313@gmail.com>
2022-05-24 12:37:09 +0300

    Удалено: цвет и операция вывода сложения a и b

--- ООП/ООП/Program.cs
+++ ООП/ООП/Program.cs
@@ -2,5 +2,3 @@
 double a = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите число b = ");
 double b = Convert.ToDouble(Console.ReadLine());
-Console.BackgroundColor = ConsoleColor.Green;
-Console.Write($"Сумма a и b равна {a+b}");
