b95296733693c39760a5f4eac5e943f7cb2c20ba
lilmen <lilmen1313@gmail.com>
2022-05-24 11:39:39 +0300

    Color
    Добавлен цвет шрифта: красный

--- ООП/ООП/Program.cs
+++ ООП/ООП/Program.cs
@@ -2,6 +2,7 @@
 double a = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите число b = ");
 double b = Convert.ToDouble(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine($"Сумма a и b равна {a+b}");
 Console.WriteLine($"Произведение a и b равно {a*b}");
 Console.WriteLine($"Частное a и b равно {a/b}");
