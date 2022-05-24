2bc3c46cb369fe51f10e93ee16bb7fe9e5f6233c
lilmen <lilmen1313@gmail.com>
2022-05-24 11:47:48 +0300

    ColorGreen
    Добавлен цвет шрифта: зеленый

--- ООП/ООП/Program.cs
+++ ООП/ООП/Program.cs
@@ -2,4 +2,5 @@
 double a = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите число b = ");
 double b = Convert.ToDouble(Console.ReadLine());
+Console.BackgroundColor = ConsoleColor.Green;
 Console.Write($"Сумма a и b равна {a+b}");
